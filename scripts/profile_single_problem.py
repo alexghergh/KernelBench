11d7c170dc0e5624dd3bde5a9391858ad3c4341e
Anne Ouyang <anne.ouyang@gmail.com>
2025-04-10 23:49:49 +0000

    save profiling script

--- scripts/profile_single_problem.py
+++ scripts/profile_single_problem.py
@@ -8,7 +8,17 @@ def run_ncu(ncu_command: str):
     return output
 
 def main():
-    ncu_command = "sudo -E /usr/local/cuda-12.2/bin/ncu --target-processes all env PATH="$PATH" python3 scripts/run_single_arch.py"
+    metrics = [
+        "dram__bytes_read.sum",
+        "dram__bytes_write.sum",
+        "dram__throughput.avg.pct_of_peak_sustained_elapsed",
+        "dram__throughput.avg.pct_of_peak_sustained_active",
+        # "dram__bytes_read.sum.peak_sustained_active",
+        # "dram__bytes_read.sum.peak_sustained_elapsed",
+        # "dram__bytes_write.sum.peak_sustained_active",
+    ]
+    metrics_str = ",".join(metrics)
+    ncu_command = f"sudo -E env PATH=\"$PATH\" /usr/local/cuda-12.2/bin/ncu --target-processes all --launch-skip 5 --launch-count 1 --kernel-name \"matmul_kernel\" --metrics {metrics_str} python3 scripts/run_single_arch.py"
     run_ncu(ncu_command)
 
 if __name__ == "__main__":
